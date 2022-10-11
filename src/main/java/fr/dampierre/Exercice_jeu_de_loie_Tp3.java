ed77877db92a3a65321af5b64ce1aab60ded3358
Baltars945 <yann.laurent@ltpdampierre.fr>
2022-10-11 11:59:23 +0200

    repeat until win

--- src/main/java/fr/dampierre/Exercice_jeu_de_loie_Tp3.java
+++ src/main/java/fr/dampierre/Exercice_jeu_de_loie_Tp3.java
@@ -3,17 +3,21 @@ import java.util.Random;
 
 public class Exercice_jeu_de_loie_Tp3 {
     public static void main(String[] args) {
-    int valeur = 0;
-    for (int i=0; i < 5 ; i++) {
+    int win = 0;
+    while (win != 1){ 
+        
+        int valeur = 0;
+        for (int i=0; i < 5 ; i++) {
         Random generateur = new Random();
         int lancer = generateur.nextInt(6) + 1;
-        System.out.println(lancer);
+        System.out.println("vous êtes à la case " + lancer);
         valeur = valeur + lancer;
     }
     if (valeur == 20) {
         System.out.println("Vous avez gagné !");
+        win = 1; 
     }  else {
         System.out.println("Vous avez perdu !");
     }
-    }
+    }}
 }
