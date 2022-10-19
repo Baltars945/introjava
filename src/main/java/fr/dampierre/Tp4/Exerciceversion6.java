57bc6a300cf9165c1ec2bf77b10392fcb9dac3ce
Baltars945 <yann.laurent@ltpdampierre.fr>
2022-10-19 18:57:43 +0200

    Version 5 6 7

---
+++ src/main/java/fr/dampierre/Tp4/Exerciceversion6.java
@@ -0,0 +1,27 @@
+package fr.dampierre.Tp4;
+import java.util.Scanner;
+public class Exerciceversion6 {
+    public static void main(String[] args) {
+        Scanner Clavier = new Scanner(System.in);
+        boolean win = false;
+        int nombre = 0 + (int) (Math.random() * 99);
+        System.out.println("Je pense à un nombre et j'essaie de deviner sans tricher.");
+        int tentatives = 0;
+        while (win != true) {
+            tentatives += 1;
+            int joueur = 0 + (int) (Math.random() * 99);
+            System.out.println("Je tente " + joueur);
+
+            if (joueur < nombre) {
+                System.out.println("Trop petit !");
+            } else if (joueur > nombre) {
+                System.out.println("Trop grand ! ");
+            } else {
+            System.out.println("gagné!");
+            System.out.println("Il m'a fallu " + tentatives + " tentatives");
+            win = true;
+            }
+        }
+        Clavier.close();
+    }
+}
