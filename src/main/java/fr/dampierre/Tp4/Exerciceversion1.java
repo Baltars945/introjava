3539a07ed3ddf9933a3e27aded9cc2a2288bd1c0
Baltars945 <yann.laurent@ltpdampierre.fr>
2022-11-22 11:43:20 +0100

    ajout de méthode

--- src/main/java/fr/dampierre/Tp4/Exerciceversion1.java
+++ src/main/java/fr/dampierre/Tp4/Exerciceversion1.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 
 public class Exerciceversion1 {
     public static void main(String[] args) {
-        
-    Scanner Clavier = new Scanner(System.in);
-    int nombre = 0 + (int) (Math.random() * 99);
-
-    System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");
-    System.out.println("Entrez un nombre :") ;
-    int joueur = Clavier.nextInt();
-    System.out.println("Vous proposez :" + joueur);
-    System.out.println("Le nombre auquel je pensais était :" + nombre);
-    if (joueur != nombre) {
-        System.out.println("Vous étiez à " + (nombre-joueur) + " de la bonne réponse.");
+        System.out.println("Je pense à un nombre entre 1 et 100 inclus. Devinez lequel.");    
+        int nombre = 0 + (int) (Math.random() * 99);
+        jeu(nombre);
+        }
+        public static void jeu(int nombre){
+        Scanner Clavier = new Scanner(System.in);
+    
+        System.out.println("Entrez un nombre :") ;
+        int joueur = Clavier.nextInt();
+        System.out.println("Vous proposez :" + joueur);
+        System.out.println("Le nombre auquel je pensais était :" + nombre);
+        if (joueur != nombre) {
+            System.out.println("Vous étiez à " + (nombre-joueur) + " de la bonne réponse.");
+        }
+        Clavier.close();
     }
-    Clavier.close();
     }
-}
