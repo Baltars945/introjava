2e3e878f0897c0dd2c648329f64c14434e522564
Baltars945 <yann.laurent@ltpdampierre.fr>
2022-11-24 08:53:45 +0100

    exo 9

--- src/main/java/fr/dampierre/Tp4/Exerciceversion9.java
+++ src/main/java/fr/dampierre/Tp4/Exerciceversion9.java
@@ -1,33 +1,34 @@
 package fr.dampierre.Tp4;
+import java.security.Principal;
 import java.util.Random;
 import java.util.Scanner;
 public class Exerciceversion9 {
     public static void main(String[] args) {
-        Random random = new Random();
-        double moyenne = 0;
-        int nbParties = 100000000;
-        int nbtentatives = 0;
-        for (int i = 1; i< nbParties; i++){
-            int nombre = random.nextInt(100) + 1;
-            int joueur = 0;
-            int max = 100;
-            int min = 1;
-            int tentatives = 0;
+        Scanner Clavier = new Scanner(System.in);
+        System.out.println("Donner le nombre de parties.");
+        int nbpartie = Clavier.nextInt();
+        int tentatives = 0;
+        float moyenne = 0;
+        for ( int i = 0; i < nbpartie ; i++) {
+            int nombrealéatoire = 0 + (int) (Math.random() * 99);
             boolean win = false;
-            while (win == false) {
+            int joueur = 0 + (int) (Math.random() * 99);
+            while (win != true) {
                 tentatives += 1;
-                joueur =  (min + max) / 2;
-                if (joueur == nombre) {
+                int min = 0;
+                int max = 99;
+                if (joueur ==  nombrealéatoire) {
                     win = true;
-                    nbtentatives += tentatives;
-                } else if (joueur > nombre) {
-                    max = joueur - 1;
+                } else if(nombrealéatoire < joueur) {
+                    max = joueur;
+                    joueur = min + (int) (Math.random() * (max-min));
                 } else {
-                    min = joueur + 1;
+                    min =joueur;
+                    joueur = min +(int) (Math.random() * (max-min));
                 }
-        }}
-        moyenne = nbtentatives / nbParties;
-        System.out.println("En moyenne, sur 10000 parties, il m'a fallu " + moyenne + " tentatives.");
-    }
+            }
+        }
+        moyenne = tentatives / nbpartie;
+        System.out.println("En moyenne, sur " + nbpartie + " parties, il m'a fallu en moyenne : " + moyenne);
 }
-    
+}
