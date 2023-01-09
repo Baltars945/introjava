b49bf888c9194df346b8294cbda2da214049c34f
Baltars945 <yann.laurent@ltpdampierre.fr>
2023-01-09 16:17:43 +0100

    test

--- src/main/java/fr/dampierre/Tp4/Exerciceversion9.java
+++ src/main/java/fr/dampierre/Tp4/Exerciceversion9.java
@@ -11,24 +11,29 @@ public class Exerciceversion9 {
         float moyenne = 0;
         for ( int i = 0; i < nbpartie ; i++) {
             int nombrealéatoire = 0 + (int) (Math.random() * 99);
-            boolean win = false;
             int joueur = 0 + (int) (Math.random() * 99);
-            while (win != true) {
-                tentatives += 1;
-                int min = 0;
-                int max = 99;
-                if (joueur ==  nombrealéatoire) {
-                    win = true;
-                } else if(nombrealéatoire < joueur) {
-                    max = joueur;
-                    joueur = min + (int) (Math.random() * (max-min));
-                } else {
-                    min =joueur;
-                    joueur = min +(int) (Math.random() * (max-min));
-                }
-            }
+            tentatives = tentatives + jeu(nombrealéatoire,joueur);    
         }
         moyenne = tentatives / nbpartie;
         System.out.println("En moyenne, sur " + nbpartie + " parties, il m'a fallu en moyenne : " + moyenne);
 }
+    public static int jeu(int nombrealéatoire,int joueur){
+        boolean win = false;    
+        int min = 0;
+        int max = 99;
+        int tentatives = 0;
+        while (win != true) {
+            tentatives += 1;
+        if (joueur ==  nombrealéatoire) {
+            win = true;
+        } else if(nombrealéatoire < joueur) {
+            max = joueur;
+            joueur = min + (int) (Math.random() * (max-min));
+        } else {
+            min =joueur;
+            joueur = min +(int) (Math.random() * (max-min));
+        }
+    }
+    return tentatives;
+    }
 }
---
+++ src/main/java/fr/dampierre/Tp4/test.java
@@ -0,0 +1,5 @@
+package fr.dampierre.Tp4;
+import java.util.Scanner;
+public class test {
+    public static void boi(String[] args) {
+    }}
